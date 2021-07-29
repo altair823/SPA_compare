8bda7672672ec59c1c704f0bfee750c78c8f069d
altair823 <rlaxogus823@naver.com>
2021-07-29 09:28:34 +0900

    A* 휴리스틱 수정

--- rawdata/MakeExcel.py
+++ rawdata/MakeExcel.py
@@ -1,18 +1,19 @@
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 
-filename = "time_result"
+filename = "100 200x200_dik^dikpq^dikbq^aspq^asbq_sd2"
 
-write_wb = Workbook()
+write_wb = load_workbook('excel/'+filename+'.xlsx')
 
 write_ws = write_wb.active
 write_ws.append(['DIK', 'DIKPQ', 'DIKBQ', 'ASPQ', 'ASBQ', 'DIK', 'DIKPQ', 'DIKBQ', 'ASPQ', 'ASBQ'])
-with open(filename + '.txt') as resultFile:
+with open('rawtext/' + filename + '.txt') as resultFile:
     lines = resultFile.readlines()
     resultDataList = {}
     resultRound = 0
     resultLength = 0
     resultTime = 0
     resultType = ''
+    currentRow = 2
     for line in lines:
         if line[:8] == "[Round: ":
             resultRound = int(line[8:-2])
@@ -34,7 +35,9 @@ with open(filename + '.txt') as resultFile:
                 allData.append(resultDataList[Type][0])
             for Type in resultDataList.keys():
                 allData.append(resultDataList[Type][1])
-            write_ws.append(allData)
+            for i in range(0, len(allData)):
+                write_ws.cell(row=currentRow, column=i+1).value = allData[i]
             resultDataList.clear()
+            currentRow += 1
 
-write_wb.save(filename + '.xlsx')
+write_wb.save('excel/' + filename + '.xlsx')
